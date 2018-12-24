2a8b9f7be856874af2b894fb45cd9d80b445da3f
HUNGDT <hk2t.thaihung@gmail.com>
2018-12-24 08:46:15 +0700

    exercise done

---
+++ src/main/java/com/hk2t/TestServlet.java
@@ -0,0 +1,21 @@
+package com.hk2t;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class TestServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        Date today = new Date();
+
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+    }
+}
